c7a1134fa47370023be5726926b66f7eacd36917
Rinke Hoekstra <rinke.hoekstra@vu.nl>
2015-07-17 10:31:35 +0200

    Logging to info

--- iribaker/__init__.py
+++ iribaker/__init__.py
@@ -4,7 +4,7 @@ import urlparse
 import logging
 
 logger = logging.getLogger(__name__)
-logger.setLevel(logging.DEBUG)
+logger.setLevel(logging.INFO)
 ch = logging.StreamHandler()
 ch.setLevel(logging.INFO)
 logger.addHandler(ch)
