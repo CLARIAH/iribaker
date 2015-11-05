76b89f2ac323e58120af003e76b9b0f6e52b7246
Rinke Hoekstra <rinke.hoekstra@vu.nl>
2015-11-05 16:08:58 +0100

    Updated version

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='iribaker',
-      version='0.1',
+      version='0.1.1',
       description='Safely convert IRI-like string to IRI.\nReplaces invalid charactes with an underscore (i.e. does not support roundtripping).\nFalls back to standard URL percent encoding.',
       url='http://github.com/clariah-sdh/iribaker',
       author='Rinke Hoekstra (VU University Amsterdam/University of Amsterdam)',
